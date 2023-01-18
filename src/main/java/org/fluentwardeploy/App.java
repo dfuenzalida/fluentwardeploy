99ca9e351f620882b6cb2b5060816b1dc2349073
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2023-01-18 13:33:15 -0800

    Update to deploy to JBoss on App Service using async APIs

--- src/main/java/org/fluentwardeploy/App.java
+++ src/main/java/org/fluentwardeploy/App.java
@@ -1,6 +1,7 @@
 package org.fluentwardeploy;
 
 import java.io.File;
+import java.util.Date;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.HttpLogDetailLevel;
@@ -16,9 +17,19 @@ import com.azure.resourcemanager.appservice.models.RuntimeStack;
 import com.azure.resourcemanager.appservice.models.WebApp;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
 
+import reactor.core.publisher.Mono;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // Configuration
+        String warFilePath = "/temp/pet-clinic-war/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.war";
+        Region myRegion = Region.CANADA_CENTRAL;
+        String suffix = "" + (100 + (int) (Math.random() * 1000)); // 3-digit suffix to avoid name collisions
+        String resourceGroupName = "fluentwardeploy" + suffix + "-rg";
+        String webAppName = "fluentwardeploy-app" + suffix;
+
+        // Create resource group and web app
         TokenCredential credential = new EnvironmentCredentialBuilder()
             .authorityHost(AzureAuthorityHosts.AZURE_PUBLIC_CLOUD)
             .build();
@@ -29,25 +40,29 @@ public class App {
             .authenticate(credential, profile)
             .withDefaultSubscription();
 
-        final ResourceGroup resourceGroup = azureResourceManager.resourceGroups().define("fluentwardeploy-rg")
-            .withRegion(Region.US_WEST2)
+        final ResourceGroup resourceGroup = azureResourceManager.resourceGroups().define(resourceGroupName)
+            .withRegion(myRegion)
             .create();
 
-        final WebApp webApp = azureResourceManager.webApps().define("fluentwardeploy-app")
-            .withRegion(Region.US_WEST2)
+        final WebApp webApp = azureResourceManager.webApps().define(webAppName)
+            .withRegion(myRegion)
             .withExistingResourceGroup(resourceGroup)
-            .withNewLinuxPlan(PricingTier.PREMIUM_P1V2)
-            .withBuiltInImage(RuntimeStack.TOMCAT_8_5_JAVA11)
+            .withNewLinuxPlan(PricingTier.PREMIUM_P1V3)       // This SKU is required to deploy JBoss
+            .withBuiltInImage(RuntimeStack.JBOSS_EAP_7_JAVA8) // NOTE: Currently maps to JBoss 7.4.2
             .create();
         
-        // File to be deployed
-        webApp.deploy(DeployType.WAR, new File("/temp/pet-clinic-war/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.war"));
+        // Async deployment API
+        Mono<Void> deployment = webApp.deployAsync(DeployType.WAR, new File(warFilePath));
 
-        // System.out.println("Waiting before restart...");
-        // Thread.sleep(3 * 1000);
-        // System.out.println("Restarting...");
+        // Deploy and block, then restart and block
+        System.out.println("*** Deployment started at " + new Date());
+        deployment.block();
+        System.out.println("*** Deployment finished at " + new Date());
 
-        webApp.restart();
+        System.out.println("*** Restarting at " + new Date());
+        webApp.restartAsync().block();
+        System.out.println("*** Restarting finished at " + new Date());
+        System.out.println("*** Browse the app at https://" + webAppName + ".azurewebsites.net/");
 
         }
 }
